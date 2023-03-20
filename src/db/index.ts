06e009be4729dec019f26bdc59024c76de80d00f
Connor Barr <crnbarr@gmail.com>
2023-03-20 15:21:39 +0000

    Removed DB URI

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,10 +3,8 @@ import clc from "cli-color";
 export * from "./ADO";
 export * from "./CodeID";
 
-// process.env.DB_URI || "mongodb://localhost:27017/andromeda-indexer";
 const DB_URI =
-  process.env.DB_URI ||
-  "mongodb+srv://andromeda:ImEZ4m2FVON6gXYq@cluster0.j3g8rhl.mongodb.net/andromeda-indexer?retryWrites=true&w=majority";
+  process.env.DB_URI || "mongodb://localhost:27017/andromeda-indexer";
 
 async function connect() {
   console.info(clc.yellow(`Connecting to Database: ${DB_URI}`));
